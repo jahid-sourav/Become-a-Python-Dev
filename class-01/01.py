0a7224d8acaf8fd8965218d01d2ea09e87ba763e
jahid-sourav <mailtojahidulislam.dev@gmail.com>
2025-08-12 13:18:20 +0600

    Init

---
+++ class-01/01.py
@@ -0,0 +1,28 @@
+#Print
+print("Hello, World!", end=" ")
+print("This is a Python script.")
+print("It prints messages to the console.")
+print("End of script.")
+
+#Input
+user_input = input("Please enter your name: ")
+print("You entered:", user_input)
+
+# Data Types
+x = 5
+print("The value of x is:", x)
+print("The type of x is:", type(x))
+x = 'Hello'
+print("The value of x is now:", x)
+print("The type of x is:", type(x))
+
+'''
+Multi-line comment
+------------------
+lorem
+ipsum dolor sit amet, consectetur adipiscing elit.
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+'''
