212c9947717de6f47e87077b30947e9495ffe545
jahid-sourav <mailtojahidulislam.dev@gmail.com>
2025-09-30 12:03:33 +0600

    Class 09 Added

---
+++ Days/09/main.py
@@ -0,0 +1,10 @@
+import math as m
+import calculate
+# from math import sqrt, pow
+print(m.sqrt(16))
+print(m.pow(2, 3))
+
+print(calculate.add(5, 3))
+print(calculate.subtract(5, 3))
+print(calculate.multiply(5, 3))
+print(calculate.divide(5, 3))
